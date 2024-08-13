18f5e488ea8b756ec11e9da5669b8d952675e304
himanshu6388 <himanshuverma8808613782@gmail.com>
2024-08-13 23:06:44 +0530

    counter app complete

---
+++ Counter/script.js
@@ -0,0 +1,11 @@
+
+let count  =0;
+function increment(){
+    count++;
+    document.getElementById('count').textContent = count;
+
+}
+function decrement(){
+    count--;
+    document.getElementById('count').textContent = count;
+}
